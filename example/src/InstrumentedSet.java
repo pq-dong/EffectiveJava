db2aca1c97d1b2ee2bcc0be9980b8dcae7832482
pengqd <pengqd@rd.netease.com>
2020-07-05 17:20:35 +0800

    MOD:第五章

---
+++ example/src/InstrumentedSet.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+// Wrapper class - uses composition in place of inheritance  (Page 90)
+public class InstrumentedSet<E> extends ForwardingSet<E> {
+  private int addCount = 0;
+
+  public InstrumentedSet(Set<E> s) {
+    super(s);
+  }
+
+  @Override public boolean add(E e) {
+    addCount++;
+    return super.add(e);
+  }
+  @Override public boolean addAll(Collection<? extends E> c) {
+    addCount += c.size();
+    return super.addAll(c);
+  }
+  public int getAddCount() {
+    return addCount;
+  }
+
+  public static void main(String[] args) {
+    InstrumentedSet<String> s = new InstrumentedSet<>(new HashSet<>());
+    s.addAll(Arrays.asList("Snap", "Crackle", "Pop"));
+    List<Object> a = new LinkedList<>();
+    List<Object> b = new LinkedList<>();
+    a.add(b);
+    a.add("xxx");
+    List<?> c = a;
+    c.remove("cc");
+    System.out.println(s.getAddCount());
+  }
+}
